f4456d7088895430d428bc83785acedd7f272c11
P4PER <timon.wegener@gmail.com>
2026-07-04 02:19:26 +0200

    style(web): position toasts at top-center

--- app/web/src/components/ui/sonner.tsx
+++ app/web/src/components/ui/sonner.tsx
@@ -2,5 +2,5 @@
 import { Toaster as Sonner } from 'sonner'
 
 export function Toaster(props: React.ComponentProps<typeof Sonner>) {
-  return <Sonner theme="dark" richColors position="bottom-right" {...props} />
+  return <Sonner theme="dark" richColors position="top-center" {...props} />
 }
